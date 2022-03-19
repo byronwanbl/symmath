e23c8bc3c7f98eacfe94c8048f2a71db54ac8deb
byronwan <byronwan@outlook.com>
2022-03-20 07:56:15 +0800

    Release v0.1.0.0

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,3 +1,13 @@
 # Changelog for symmath
 
-## Unreleased changes
+## 0.1.0.0
+
+- Representation of Expression.
+- Integral expression operation and simplication.
+
+## 0.2.0.0 (TODO)
+
+- Use **typeclass** to replace ADT `Expr`.
+- Add **Metadata** to `Expr`
+- Use more **efficient** methods to simplify.
+- Support **factorization**, **fraction** and **radical expression**.
--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,24 @@ module Main where
 import Control.Category ((>>>))
 import Data.Functor (fmap)
 import Data.Maybe (maybe)
+import Foreign.C (CString, newCString, peekCString)
 import Symmath.Lib (parseExpr, simplify)
 import Prelude
-import Foreign.C (CString, peekCString, newCString)
 
 main :: IO ()
-main = getLine >>= (simplify' >>> putStrLn) >> main
+main = do
+  putStrLn "Symmath CLI"
+  putStrLn "Copyright 2022 ByronWan. All rights reserved."
+  putStrLn ""
+  putStrLn "Input expressions below to simplify them:"
+  main'
+
+main' :: IO ()
+main' = do
+  s <- getLine
+  let s' = simplify' s
+  putStrLn s'
+  main'
 
 simplify' :: String -> String
 simplify' = parseExpr >>> fmap simplify >>> maybe "Simplify failed!" show
@@ -16,4 +28,4 @@ simplify' = parseExpr >>> fmap simplify >>> maybe "Simplify failed!" show
 simplifyC :: CString -> IO CString
 simplifyC x = peekCString x >>= (simplify' >>> newCString)
 
-foreign export ccall simplifyC:: CString -> IO CString
+foreign export ccall simplifyC :: CString -> IO CString
